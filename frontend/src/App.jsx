4e8f96a89797e3df01ba415cb2d11751c9228662
Idil Mahamed <mahamed.idil@gmail.com>
2024-01-11 20:48:43 -0500

    isolating components,displaying photo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,44 @@ import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
+import FavBadge from 'components/FavBadge';
+import FavIcon from 'components/FavIcon';
+import PhotoFavButton from 'components/PhotoFavButton';
+import PhotoList from 'components/PhotoList';
+import TopicList from 'components/TopicList';
+import TopicListItem from 'components/TopicListItem';
+import TopNavigation from 'components/TopNavigationBar';
 
 // Note: Rendering a single component to build components in isolation
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
+
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem  
+       id={sampleDataForPhotoListItem.id}
+       location={sampleDataForPhotoListItem.location}
+       imageSource={sampleDataForPhotoListItem.imageSource}
+       username={sampleDataForPhotoListItem.username}
+       profile={sampleDataForPhotoListItem.profile}
+     />
+      { /*FavBadge/> */}
+      {/*FavIcon/> */}
+      { /* PhotoFavButton/>*/}
+      <PhotoList/> 
+      {/*TopicList/>*/}     
+       {/*TopicListItem/>*/}
+      {/*TopNavigation/>*/}
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,21 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+
+const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
+  return (
+    <div className="photo-list-item">
+      <img src={imageSource} alt={`Photo ${id}`} />
+      <div className="photographer-info">
+        <img src={profile} alt={`${username}'s profile`} />
+        <span className="username">{username}</span>
+      </div>
+      <div className="photo-location">
+        {location.city}, {location.country}
+      </div>
+    </div>
+  );
 };
 
-const PhotoListItem = () => {
-  /* Insert React */
-};
 
 export default PhotoListItem;
