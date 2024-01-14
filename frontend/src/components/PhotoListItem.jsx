76dc5c68910c299f6183417cdb6df15a415f23bb
Idil Mahamed <mahamed.idil@gmail.com>
2024-01-14 03:02:49 -0500

    Add blank modal when users click a photo

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+
 
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from './PhotoFavButton';
+import PhotoDetailsModal from '../routes/PhotoDetailsModal';
+import React, { useState } from 'react';
 
 
 
 const PhotoListItem = ({ id, location, imageSource, username, profile,isFavorited, onToggleFavorite  }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
   return (
     <div className="photo-list__item">
-      <img src={imageSource} alt={`Photo ${id}`} className="photo-list__image" />
+      <img src={imageSource} alt={`Photo ${id}`} className="photo-list__image" onClick={toggleModal}  />
       <div className="photo-list__user-details">
         <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile" />
         <div className="photo-list__user-info">
@@ -19,6 +26,7 @@ const PhotoListItem = ({ id, location, imageSource, username, profile,isFavorite
         </div>
       </div>
       <PhotoFavButton isLiked={isFavorited} onToggle={onToggleFavorite}  />
+      {isModalOpen && <PhotoDetailsModal />}
     </div>
   );
 };
